967fc5095663f07b28d68c704766633bc4551406
Girish Mudigonda <mudigondagirish97@gmail.com>
2024-12-16 10:23:03 +0530

    added 101 pro

--- src/main/java/mavenlearn/GirishLearn.java
+++ src/main/java/mavenlearn/GirishLearn.java
@@ -3,7 +3,11 @@ package mavenlearn;
 public class GirishLearn {
 	public static void main(String[] args) {
 		System.out.println("girish");
+<<<<<<< Updated upstream
 		System.out.println("girish 202");
+=======
+		System.out.println("girish 101");
+>>>>>>> Stashed changes
 	}
 
 }
